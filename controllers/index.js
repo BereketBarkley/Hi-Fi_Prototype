47b1623caeffaf2b4ebd88502b51ff4c6a8774d5
emmettchen23 <emmett.chen23@trinityschoolnyc.org>
2023-03-14 11:45:50 -0400

    sdas
    asds

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const express = require('express'),
+  router = express.Router();
+
+const E= require('../models/consumer_model');
+module.exports = router;
--- controllers/staff_controller.js
+++ controllers/staff_controller.js
@@ -1,1 +1,6 @@
+const express = require('express'),
+  router = express.Router();
+
+const Staff = require('../models/consumer_model');
+
 module.exports = router;
