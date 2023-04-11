c3a7d4e4b7ddb43bc6ea3eb35293b2db4376fd25
emmettchen23 <emmett.chen23@trinityschoolnyc.org>
2023-04-10 21:47:05 -0400

    ssadsa
    jkjjk

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,8 +57,20 @@ router.get('/auth/google/callback',
     failureRedirect: '/error?code=401'
   }),
   function(request, response) {
+
     console.log(userProfile);
-    response.redirect('/');
+    let a = request.user._json.email;
+    console.log("a");
+    let b = a.split("");
+    if(a == "justin.godhe@trinityschoolnyc.org"){
+      response.redirect('/adminPage');
+    }
+    for(let i = 0; i< b.length; i++){
+      if(b[i] == "1" || b[i] == "2" || b[i] == "3" || b[i] == "4" || b[i] == "5" || b[i] == "6" || b[i] == "7" || b[i] == "8" || b[i] == "9"){
+        response.redirect('/profile')
+      }
+    }
+    response.redirect('/orderFulfillment');
   });
 
 router.get("/auth/logout", (request, response) => {
